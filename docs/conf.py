89e75b133b69bd93a70bc2aa43580ec16cc9fab7
Tahir Mushtaq <tahirmushtaq5@gmail.com>
2021-06-10 22:57:03 +0500

    conf

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+# for Sphinx-1.4 or newer
+extensions = ['recommonmark']
+
+# for Sphinx-1.3
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
+source_suffix = ['.rst', '.md']
