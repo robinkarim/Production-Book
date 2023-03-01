d16a3b19e29940452ce37cd74d88d241532e7cdf
vivek-infineon <117719172+vivek-infineon@users.noreply.github.com>
2023-03-01 21:16:16 +0530

    Added html_baseurl
    At line no. 132, 133

--- docs/conf.py
+++ docs/conf.py
@@ -129,6 +129,9 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
 
+# Additional hatml_basesurl set by Vivek for sitemap
+html_baseurl = 'http://aus-opndoc401.cypress.com/docs/'
+
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
 #html_title = None
