06169c2c243abd9ba1e75a5659f60062b5b4c66a
vivek-infineon <117719172+vivek-infineon@users.noreply.github.com>
2023-03-01 21:39:21 +0530

    Commented html_baseurl line
    To undo build error

--- docs/conf.py
+++ docs/conf.py
@@ -130,7 +130,7 @@ html_theme = 'sphinx_rtd_theme'
 #html_theme_path = []
 
 # Additional hatml_basesurl set by Vivek for sitemap
-html_baseurl = '/docs/'
+#html_baseurl = '/docs/'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
